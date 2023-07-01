b40d1199ada94142f907c94c27aca5226161f520
unknown <spryaratrik.basak@gmail.com>
2023-07-01 09:37:01 +0530

    Added db connection

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://admin-aratrik:bdroZShNksUVxBcG@cluster0.zhojb.mongodb.net/blogDB");
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(()=>{
+    console.log("Connected to MongoDB");
+}).catch((err)=>{
+    console.log(err);
+})
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -87,20 +96,12 @@ app.post("/delete",function(req,res){
     else{
       console.log(err);
     }
-  // Or use below code both works
-  // Post.findByIdAndRemove(requestedPostId, function(err){
-  //   if(err){
-  //     console.log(err);
-  //   } else {
-  //     console.log("Successfully deleted checked item.");
-  //     res.redirect("/");
-  //   }
   });
 })
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 3000;
+  port = 4000;
 }
  
 app.listen(port, function() {
